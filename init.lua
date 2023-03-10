5f52cbdd3fa790c9fdc1b8e2e113798b40cfb8ae
Eivind <eivind@pop-os.localdomain>
2023-03-10 19:11:24 +0100

    boom

---
+++ init.lua
@@ -0,0 +1,6 @@
+require "user.options"
+require "user.plugins"
+--require "user.treesitter"
+require "user.keymaps"
+require "user.comment"
+require "user.toggleterm"
---
+++ lua/user/keymaps.lua
@@ -0,0 +1,45 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+keymap("n", "<leader>p", ":w | :! pdflatex %<CR><CR> | :! biber %:r <CR> | :! pdflatex %<CR><CR>", opts)
+
+vim.cmd [[map <leader>z :! zathura %<.pdf &<CR><CR>]]
+vim.cmd [[map <leader>p :w \| :! pdflatex %<CR><CR> \| :! biber %:r <CR> \| :! pdflatex %<CR><CR>]]
+vim.cmd [[map <leader>c :%y+<CR>]]
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local insert_cite = Terminal:new({ cmd = "insert_cite", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+function _insert_cite_toggle()
+  insert_cite:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>s", "<cmd>lua _insert_cite_toggle()<CR>", {noremap = true, silent = true})
+
+-- Visual mode --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "A-j", ":m .+1<CR>==", opts)
+keymap("v", "A-k", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+
